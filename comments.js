6519f355b69fd2d236cc65c405efe6fc14ba250d
Silva <adsonplaygg@gmail.com>
2023-11-16 23:35:37 -0300

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,82 @@
-import http from 'http';
+//Create web server
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var fs = require("fs");
+var cors = require('cors');
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var path = require('path');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Comment = require('./models/Comment.js');
+var Post = require('./models/Post.js');
+var User = require('./models/User.js');
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost:27017/comments');
 
-const server = http.createServer((_req, res) => {
-    res.statusCode;
-
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
+//Get all comments
+router.route('/comments').get(function (req, res) {
+    Comment.find(function (err, comments) {
+        if (err) {
+            return res.send(err);
+        }
+        res.json(comments);
+    });
 });
 
-server.listen(3000, () => {
-    console.log('Server running on port 3000');
+//Get comments by post id
+router.route('/comments/:id').get(function (req, res) {
+    Comment.find({postID: req.params.id}, function(err, comments) {
+        if (err) {
+            return res.send(err);
+        }
+        res.json(comments);
+    });
 });
+
+//Post a comment
+router.route('/comments').post(function (req, res) {
+    var comment = new Comment(req.body);
+    comment.save(function (err) {
+        if (err) {
+            return res.send(err);
+        }
+        res.send({ message: 'Comment Added' });
+    });
+});
+
+//Update a comment
+router.route('/comments/:id').put(function(req, res) {
+    Comment.findOne({ _id: req.params.id }, function (err, comment) {
+        if (err) {
+            return res.send(err);
+        }
+        for (prop in req.body) {
+            comment[prop] = req.body[prop];
+        }
+        comment.save(function(err) {
+            if (err) {
+                return res.send(err);
+            }
+            res.json({ message: 'Comment updated!' });
+        });
+    });
+});
+
+//Delete a comment
+router.route('/comments/:id').delete(function(req, res) {
+    Comment.remove({
+        _id: req.params.id
+    }, function(err, comment) {
+        if (err) {
+            return res.send(err);
+        }
+        res.json({ message: 'Successfully deleted' });
+    });
+});
+
+//Get all posts
+router.route('/posts').get(function (req, res) {
+    Post.find(function (err, posts) {
